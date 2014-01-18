a38293b8094ea163c6c792c5da950a6df51bebc3
CHENBINGZHOU <bingzhou.chen@philips.com>
2014-01-19 00:00:44 +0100

    1st

---
+++ sample.js
@@ -0,0 +1,22 @@
+src='https://cdn.firebase.com/v0/firebase.js'
+src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'
+ var myDataRef = new Firebase('https://nestlabs-neighbour.firebaseio.com/users/13/structures/f6ab7cf0-5272-11e3-b09f-22000aecc032/');
+	  myDataRef.auth("6OzN9nzHcn5kc0wrrHTqJWIvhnlXv5JkdMmZLDg7", function(error) {
+  if(error) {
+    alert("Login Failed!", error);
+  } else {
+  }
+});
+      myDataRef.on('value', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.away,message.away);
+			$.ajax({
+						url: "http://testportalkiri3.appspot.com/Redirect",
+				type: 'get',
+
+			});
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(name).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
