bd780befb0eaf3b8a4dce4262d8a28b712a20a8e
CHENBINGZHOU <bingzhou.chen@philips.com>
2014-01-19 16:01:51 +0100

    5th

--- sample.js
+++ sample.js
@@ -1,22 +1,23 @@
- var myFirebase = require('firebase')
- var myajax = require('jquery.min')
- var myDataRef = new myFirebase.Firebase('https://nestlabs-neighbour.firebaseio.com/users/13/structures/f6ab7cf0-5272-11e3-b09f-22000aecc032/');
-	  myDataRef.auth("6OzN9nzHcn5kc0wrrHTqJWIvhnlXv5JkdMmZLDg7", function(error) {
+
+var myfirebase=require('firebase')
+var $=require('jquery')
+var http=require('http')
+var log=require('log')
+var fs=require("fs")
+var myDataRef = new myfirebase('https://nestlabs-neighbour.firebaseio.com/users/13/structures/f6ab7cf0-5272-11e3-b09f-22000aecc032/');
+          myDataRef.auth("6OzN9nzHcn5kc0wrrHTqJWIvhnlXv5JkdMmZLDg7", function(error,result) {
   if(error) {
-    alert("Login Failed!", error);
+    console.log("Login Failed!", error);
   } else {
+    console.log("Auth successfully");
   }
 });
-      myDataRef.on('value', function(snapshot) {
-        var message = snapshot.val();
-        displayChatMessage(message.away,message.away);
-			$.myajax.ajax({
-						url: "http://testportalkiri3.appspot.com/Redirect",
-				type: 'get',
-
-			});
+console.log("do something");
+      myDataRef.on('value', function(dataSnapshot) {
+        var message = dataSnapshot.val();
+console.log(message);
+http.get("http://testportalkiri3.appspot.com/Redirect");
+fs.appendFile('./a.txt',"data changed",'utf8',function(err){
+if(err){console.log("bad")}
+});
       });
-      function displayChatMessage(name, text) {
-        $('<div/>').text(name).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-      };
